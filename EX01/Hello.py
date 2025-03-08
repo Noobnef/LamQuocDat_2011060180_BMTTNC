b544792bb42fad37eef1f1a5a787362497395549
Quoc Dat <datisnoob@gmail.com>
2025-03-08 10:34:50 +0700

    add - lab 02 ex01 caesar

--- EX01/Hello.py
+++
@@ -1,3 +0,0 @@
-print("Hello")
-print("My fullname is Lam Quoc Dat")
-print("Im 18 years old")
--- EX02/EX02_02.py
+++
@@ -1,3 +0,0 @@
-banKinh = float(input("Nhập bán kính hình tròn: "))
-dienTich = 3.14 * (banKinh**2)
-print("Diện tích hình tròn là: ",dienTich)
